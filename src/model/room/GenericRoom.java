af4f25fc80a6f3feba3f841e4d2c6b4c93a8ff35
Patrick Maley <pmaley@email.arizona.edu>
2016-04-02 15:02:28 -0700

    Adds the rooms

---
+++ src/model/room/GenericRoom.java
@@ -0,0 +1,86 @@
+package model.room;
+
+public abstract class GenericRoom {
+	private GenericRoom northRoom;
+	private GenericRoom eastRoom;
+	private GenericRoom southRoom;
+	private GenericRoom westRoom;
+	private String roomTitle;
+	private String roomDescription;
+	private boolean mobPresent;
+	private boolean playerPresent;
+	private int gridX;
+	private int gridY;
+	
+	public GenericRoom(){
+		
+	}
+
+	//private ArrayList<Items> itemsList = new ArrayList<Items>();
+	public GenericRoom(GenericRoom north, GenericRoom south, GenericRoom east, GenericRoom west){
+		this.northRoom = north;
+		this.southRoom = south;
+		this.eastRoom = east;
+		this.westRoom = west;
+	}
+	
+	public GenericRoom getNextRoom(String direction){
+		switch(direction){
+		case "n": case "north":
+			if(northRoom != null) return this.northRoom;
+			else return null;
+		case "e": case "east":
+			if(eastRoom !=null) return this.eastRoom;
+			else return null;
+		case "s": case "south":
+			if(southRoom != null) return this.southRoom;
+			else return null;
+		case "w": case "west":
+			if(westRoom != null) return this.westRoom;
+			else return null;
+		default:
+			return null;
+		}
+	}
+	
+	public void setRoomDescription(String details){
+		this.roomDescription = details;
+	}
+	
+//	public boolean checkMobs(){
+//		return this.mobPresent;
+//	}
+	
+	public void setTitle(String title){
+		this.roomTitle = title;
+	}
+	
+	public void setNorthRoom(GenericRoom room){
+		this.northRoom = room;
+	}
+	
+	public void setSouthRoom(GenericRoom room){
+		this.southRoom = room;
+	}
+	
+	public void setEastRoom(GenericRoom room){
+		this.eastRoom = room;
+	}
+	
+	public void setWestRoom(GenericRoom room){
+		this.westRoom = room;
+	}
+	
+	public GenericRoom getNorthRoom(){
+		return this.northRoom;
+	}
+	public GenericRoom getSouthRoom(){
+		return this.southRoom;
+	}
+	public GenericRoom getEastRoom(){
+		return this.eastRoom;
+	}
+	public GenericRoom getWestRoom(){
+		return this.westRoom;
+	}
+}
---
+++ src/model/room/NPCRoom.java
@@ -0,0 +1,25 @@
+package model.room;
+
+public class NPCRoom extends GenericRoom{
+	private int mobsAllowed;
+	//private ArrayList<Mob> npcList = new ArrayList<Mob>();
+	public NPCRoom(GenericRoom north, 
+			GenericRoom south, 
+			GenericRoom east, 
+			GenericRoom west,
+			String roomName) {
+		super(north, south, east, west);
+		mobsAllowed = 0;
+		this.setTitle(roomName);
+	}
+	
+	public NPCRoom(
+			String roomName) {
+		super();
+		mobsAllowed = 0;
+		this.setTitle(roomName);
+	}
+	
+	//Place in getters and setters for the npc object in the room
+
+}
