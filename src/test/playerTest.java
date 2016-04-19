0559a30e7cfe300eca1d1aadbb149f9f0f9f4392
Ana Huff <ahuff13@email.arizona.edu>
2016-04-18 21:05:32 -0700

    player, playerList and tests updated

--- src/test/playerTest.java
+++ src/test/playerTest.java
@@ -18,26 +18,31 @@ import model.room.GenericRoom;
 import model.room.RoomFactory;
 import model.room.RoomType;
 
-public class playerTest {
+public class PlayerTest {
 
+	//Usable passwords
 	private char[] password1 = new char[] {'1'};
 	private char[] password2 = new char[] {'2', '2'};
 	private char[] password3 = new char[] {'3', '3', '3'};
 	
 	@Test
 	public void testPlayerGetters() throws NoSuchAlgorithmException, NoSuchProviderException {
+		//Initial list, map, and a GenericRoom
 		PlayerList current = PlayerList.setList();
 		Map currMap = Map.setMap();
 		GenericRoom nextRoom = RoomFactory.designRoom(RoomType.NPC);
 		
+		//Usable players
 		Player player1 = new Player("Lee", password1, "Doris", "Gryffindor");
 		Player player2 = new Player("Luna", password2, "Debbie", "Ravenclaw");
 		Player player3 = new Player("Neville", password3, "Kevin", "Gryffindor");
 		
+		//Usable spells
 		Spell spell1 = new AvadaKedvra();
 		Spell spell2 = new Expelliarumus();
 		Spell spell3 = new SectumSempra();
 		
+		//Tests playerList
 		assertEquals(0, current.getCurrentList().size());
 		current.newPlayer(player1);
 		assertEquals(1, current.getCurrentList().size());
@@ -48,6 +53,7 @@ public class playerTest {
 		current.newPlayer(player3);
 		assertEquals(3, current.getCurrentList().size());
 
+		//Tests username, password, and gameName
 		assertEquals("Lee", player1.getUsername());
 		assertEquals(true, player1.checkPassword(password1));
 		assertNotEquals(player1.getUsername(), player2.getUsername());
@@ -56,6 +62,16 @@ public class playerTest {
 		assertEquals("Doris", player1.getGameName());
 		assertNotEquals("Debbie", player1.getGameName());
 		
+		//Tests extraLives
+		assertEquals(0, player1.getExtraLives());
+		player1.addExtraLives(2);
+		assertEquals(2, player1.getExtraLives());
+		player1.decreaseHP(100);
+		player1.useExtraLife();
+		assertEquals(100, player1.getHP(), .0001);
+		assertEquals(1, player1.getExtraLives());
+		
+		//Tests spellbook
 		player1.addKnownSpells(spell1);
 		assertEquals(1, player1.getKnownSpells().size());
 		player1.addKnownSpells(spell2);
@@ -65,9 +81,11 @@ public class playerTest {
 		player1.removeKnownSpells(spell3);
 		assertEquals(1, player1.getKnownSpells().size());
 		
+		//Tests house getter
 		assertEquals("Gryffindor", player1.getHouse());
 		assertNotEquals("Gryffindor", player2.getHouse());
 		
+		//Tests HP getter, decrease, and increase
 		assertEquals(100, player3.getHP(), .0001);
 		player3.decreaseHP(50);
 		assertEquals(50, player3.getHP(), .0001);
@@ -82,11 +100,13 @@ public class playerTest {
 		player3.increaseHP(100);
 		assertEquals(200, player3.getHP(), .0001);
 		
+		//Tests map and room
 		assertEquals(currMap.getEntrance(), player1.getRoom());
 		player1.setCurrentRoom(nextRoom);
 		assertNotEquals(currMap.getEntrance(), player1.getRoom());
 		assertEquals(nextRoom, player1.getRoom());
 		
+		//Tests inventory
 		assertNotEquals(player1.getInventory(), player2.getInventory());
 
 	}
